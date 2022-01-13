dc9739dd2e588637cb195d3a4996636032b327da
TranDanh1122 <trandanh14042000@gmail.com>
2022-01-13 17:10:58 +0700

    update chart (temp)

--- app/src/main/java/com/example/btl_final_final/fragmentleftbar/home_Fragment.java
+++ app/src/main/java/com/example/btl_final_final/fragmentleftbar/home_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.btl_final_final.fragmentleftbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.btl_final_final.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +33,7 @@ public class home_Fragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    View rootview;
     public home_Fragment() {
         // Required empty public constructor
     }
@@ -60,7 +68,32 @@ public class home_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false);
+        rootview=inflater.inflate(R.layout.fragment_home_, container, false);
+        BarChart barChart=rootview.findViewById(R.id.barchart);
+        ArrayList<BarEntry> tongthu=new ArrayList<>();
+        tongthu.add(new BarEntry(1,100000));
+        tongthu.add(new BarEntry(2,103000));
+        tongthu.add(new BarEntry(3,1200000));
+        tongthu.add(new BarEntry(4,1600000));
+        tongthu.add(new BarEntry(5,300000));
+        tongthu.add(new BarEntry(6,700000));
+        tongthu.add(new BarEntry(7,11100000));
+        tongthu.add(new BarEntry(8,1200000));
+        tongthu.add(new BarEntry(9,200000));
+        tongthu.add(new BarEntry(10,2100000));
+        tongthu.add(new BarEntry(11,10110000));
+        tongthu.add(new BarEntry(12,28100000));
+
+        BarDataSet barDataSet=new BarDataSet(tongthu,"Tong thu");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(14f);
+        BarData barData=new BarData(barDataSet);
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Tong thu hang thang");
+        barChart.animateY(2000);
+
+        return rootview;
     }
 }
